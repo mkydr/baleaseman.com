dd4798b32dba7f85476a7061866f86c793eead4c
Your Name <you@example.com>
2024-03-17 12:55:51 +0000

    Update

---
+++ wp-content/litespeed/js/9aeb27eb05f184fd0a9d60e21b856be72fbc.js
@@ -0,0 +1,2 @@
+(function($){woodmartThemeModule.scrollTop=function(){var $scrollTop=$('.scrollToTop');woodmartThemeModule.$window.on('scroll',function(){if($(this).scrollTop()>100){$scrollTop.addClass('button-show')}else{$scrollTop.removeClass('button-show')}});$scrollTop.on('click',function(){$('html, body').animate({scrollTop:0},800);return!1})};$(document).ready(function(){woodmartThemeModule.scrollTop()})})(jQuery)
+;
---
+++ wp-content/litespeed/js/9be8414e01253a0bd6732df806dcc0ad4526.js
@@ -0,0 +1,2 @@
+(function($){woodmartThemeModule.siteScroll=function(){if(window.innerWidth>woodmartThemeModule.windowWidth){$('html').addClass('wd-scrollbar')}};$(document).ready(function(){woodmartThemeModule.siteScroll()})})(jQuery)
+;
